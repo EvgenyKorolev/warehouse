14fae0138f869f233b58d19fe44073448b5cbc88
EvgenyKorolev <79297269463@yandex.ru>
2018-12-27 15:34:36 +0300

    Чуть поправим :)

--- src/functions.h
+++ src/functions.h
@@ -1,7 +1,6 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 #include <QString>
-#include <QJsonObject>
 namespace my {
 QString base64_plus(const QString& arg);
 QString base64_minus(const QString& arg);
